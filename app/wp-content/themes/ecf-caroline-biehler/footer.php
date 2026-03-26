7335394fbc9f74d1b79a2830938271e2ed652cf1
Caroline BIEHLER <70326049+Caroline1609@users.noreply.github.com>
2026-03-26 09:34:01 +0100

    functions.php OK
    Etape 3 terminer

--- app/wp-content/themes/ecf-caroline-biehler/footer.php
+++ app/wp-content/themes/ecf-caroline-biehler/footer.php
@@ -1,5 +1,10 @@
 </main>
+<aside class="site-aside">
+    <!-- SIDEBAR -->
+    <?php dynamic_sidebar('left'); ?>
+</aside>
 
-    <?php wp_footer(); ?>
+<?php wp_footer(); ?>
 </body>
+
 </html>
---
+++ app/wp-content/themes/ecf-caroline-biehler/functions.php
@@ -0,0 +1,27 @@
+<?php
+
+function cb_add_thumbnails() {
+    add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'cb_add_thumbnails');
+
+
+function cb_menu_setup() {
+    register_nav_menus([
+        'menuPrincipal' => 'top-menu'
+    ]);
+}
+
+add_action('init', 'cb_menu_setup');
+
+function cb_sidebar_setup() {
+    register_sidebar([
+        'id' => 'left',
+        'name' => 'left-sidebar',
+        'before_widget' => '<div>',
+        'after_widget' => '</div>'
+    ]);
+}
+
+add_action('widgets_init', 'cb_sidebar_setup');
